6b90221832af794a12949ce0de1ae261ef285c4a
Jingsong Lee <jingsonglee0@gmail.com>
2021-05-27 14:16:01 +0800

    [query] Adjust q5 and q8 to include window end time (#18)

--- nexmark-flink/src/main/resources/queries/q5.sql
+++ nexmark-flink/src/main/resources/queries/q5.sql
@@ -22,7 +22,8 @@ SELECT AuctionBids.auction, AuctionBids.num
    SELECT
      B1.auction,
      count(*) AS num,
-     HOP_START(B1.dateTime, INTERVAL '2' SECOND, INTERVAL '10' SECOND) AS starttime
+     HOP_START(B1.dateTime, INTERVAL '2' SECOND, INTERVAL '10' SECOND) AS starttime,
+     HOP_END(B1.dateTime, INTERVAL '2' SECOND, INTERVAL '10' SECOND) AS endtime
    FROM bid B1
    GROUP BY
      B1.auction,
@@ -31,16 +32,20 @@ SELECT AuctionBids.auction, AuctionBids.num
  JOIN (
    SELECT
      max(CountBids.num) AS maxn,
-     CountBids.starttime
+     CountBids.starttime,
+     CountBids.endtime
    FROM (
      SELECT
        count(*) AS num,
-       HOP_START(B2.dateTime, INTERVAL '2' SECOND, INTERVAL '10' SECOND) AS starttime
+       HOP_START(B2.dateTime, INTERVAL '2' SECOND, INTERVAL '10' SECOND) AS starttime,
+       HOP_END(B2.dateTime, INTERVAL '2' SECOND, INTERVAL '10' SECOND) AS endtime
      FROM bid B2
      GROUP BY
        B2.auction,
        HOP(B2.dateTime, INTERVAL '2' SECOND, INTERVAL '10' SECOND)
      ) AS CountBids
-   GROUP BY CountBids.starttime
+   GROUP BY CountBids.starttime, CountBids.endtime
  ) AS MaxBids
- ON AuctionBids.starttime = MaxBids.starttime AND AuctionBids.num >= MaxBids.maxn;
+ ON AuctionBids.starttime = MaxBids.starttime AND
+    AuctionBids.endtime = MaxBids.endtime AND
+    AuctionBids.num >= MaxBids.maxn;
--- nexmark-flink/src/main/resources/queries/q8.sql
+++ nexmark-flink/src/main/resources/queries/q8.sql
@@ -19,13 +19,17 @@ CREATE TABLE discard_sink (
 INSERT INTO discard_sink
 SELECT P.id, P.name, P.starttime
 FROM (
-  SELECT P.id, P.name, TUMBLE_START(P.dateTime, INTERVAL '10' SECOND) AS starttime
+  SELECT P.id, P.name,
+         TUMBLE_START(P.dateTime, INTERVAL '10' SECOND) AS starttime,
+         TUMBLE_END(P.dateTime, INTERVAL '10' SECOND) AS endtime
   FROM person P
   GROUP BY P.id, P.name, TUMBLE(P.dateTime, INTERVAL '10' SECOND)
 ) P
 JOIN (
-  SELECT A.seller, TUMBLE_START(A.dateTime, INTERVAL '10' SECOND) AS starttime
+  SELECT A.seller,
+         TUMBLE_START(A.dateTime, INTERVAL '10' SECOND) AS starttime,
+         TUMBLE_END(A.dateTime, INTERVAL '10' SECOND) AS endtime
   FROM auction A
   GROUP BY A.seller, TUMBLE(A.dateTime, INTERVAL '10' SECOND)
 ) A
-ON P.id = A.seller AND P.starttime = A.starttime;
+ON P.id = A.seller AND P.starttime = A.starttime AND P.endtime = A.endtime;
